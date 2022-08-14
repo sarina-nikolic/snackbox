3d267482c4a51bd97c7ac6e76f4639a040ff69aa
Sarina Nikolić <sarina.nikolic@gmail.com>
2022-08-14 23:12:35 +0200

    Setup npm project w/ TypeScript support

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// import { getConstantValue } from "typescript";
+const userName: string = 'Sarina';
+var jsMessage: string = `Moin TypeScript Welt -- ${userName}`;
+
+console.warn(`Script run @ ${new Date()}`);
+console.log(jsMessage);
+
+const arr: Array<string> = new Array('jedan', 'dva');
+console.info(`korak 1. arr = ${arr}`);
+console.info(`korak 2. arr = ${arr.concat('tri')}`);
+arr.push('četiri')
+console.info(`korak 3. arr = ${arr}`);
+arr.push('pet', 'šest');
+console.info(`korak 4. arr = ${arr}`);
+const lastEl = arr.pop();
+console.info(`korak 5. arr = ${arr}`);
+console.info(`korak 5. lastEl = ${lastEl}`);
+
+
+//  const result = fun(... arg);
