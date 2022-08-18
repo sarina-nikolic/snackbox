87e727d437ff5b318e2652e2ea64c7b9ee634753
Sarina Nikolić <sarina.nikolic@gmail.com>
2022-08-18 15:52:06 +0200

    Update app.js: Define Snacks interface and initial functions

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-// import { getConstantValue } from "typescript";
-const userName: string = 'Sarina';
-var jsMessage: string = `Moin TypeScript Welt -- ${userName}`;
+interface Snacks {
+id: number;
+name: string;
+kcalPer100g: number;
+priceInEuro: number;
+}
 
-console.warn(`Script run @ ${new Date()}`);
-console.log(jsMessage);
+function getSnacks(): Promise<Snacks[]> {
+    return fetch('/snacks-data.json')
+    .then(res => res.json())
+    .then(res => {
+        //(parameter) res: response
+return res as Snacks[]
+})
+}
 
-const arr: Array<string> = new Array('jedan', 'dva');
-console.info(`korak 1. arr = ${arr}`);
-console.info(`korak 2. arr = ${arr.concat('tri')}`);
-arr.push('četiri')
-console.info(`korak 3. arr = ${arr}`);
-arr.push('pet', 'šest');
-console.info(`korak 4. arr = ${arr}`);
-const lastEl = arr.pop();
-console.info(`korak 5. arr = ${arr}`);
-console.info(`korak 5. lastEl = ${lastEl}`);
-
-
+const result = document.getElementById('result')
+getSnacks()
+.then(snacks => {
+    result.innerHTML = snacks.map(snack => snack.name).toString()
+})
